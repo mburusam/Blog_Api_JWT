5dfb155540a9c6a2fbe97b39b1c6fb422567d083
Samuel Mburu <samuel.mburu@tracom.co.ke>
2022-11-03 10:14:13 +0300

    Role based Database authentication

--- src/main/java/blog_app/config/SecurityConfig.java
+++ src/main/java/blog_app/config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package blog_app.config;
 
+import blog_app.Security.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -18,6 +21,9 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
 
     @Bean
     PasswordEncoder encoder (){
@@ -35,11 +41,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    @Bean
-    protected UserDetailsService userDetailsService() {
-       UserDetails sam = User.builder().username("sam").password( encoder().encode("pass")).roles("USER").build();
-        UserDetails admin =User.builder().username("admin").password(encoder().encode("pass")).roles("ADMIN").build();
-
-        return new InMemoryUserDetailsManager(sam,admin);
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(encoder());
     }
+
+    //    @Override
+//    @Bean
+//    protected UserDetailsService userDetailsService() {
+//       UserDetails sam = User.builder().username("sam").password( encoder().encode("pass")).roles("USER").build();
+//        UserDetails admin =User.builder().username("admin").password(encoder().encode("pass")).roles("ADMIN").build();
+//
+//        return new InMemoryUserDetailsManager(sam,admin);
+//    }
 }
